518dc2c5f5254bc6f1e7497048e8c2c540137650
Marcelina Wajdzik <marcelawa20@gmail.com>
2019-12-03 21:37:20 +0100

    modification in main.js - new an event and a callback added

--- Desktop/javaScript - kurs samuraj/Projekt 1/main.js
+++ Desktop/javaScript - kurs samuraj/Projekt 1/main.js
@@ -1,1 +1,16 @@
 const btnAdd = document.querySelector('button');
+let newNumber = 1;
+const addElement = () => {
+    const containerDiv = document.querySelector('.container-div')
+    let newSquare = document.createElement('div');
+    newSquare.innerText = newNumber;
+    newSquare.classList.add('square');
+    containerDiv.appendChild(newSquare)
+    if (newSquare.innerText % 5 == 0) {
+        newSquare.classList.add('circle');
+    }
+    newNumber++
+
+}
+
+btnAdd.addEventListener('click', addElement)
